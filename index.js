2ab2b6fe3cccd6576d6a78b67e35e5fe73814fb5
Jaynes <alex.jaynes@adidas-group.com>
2017-10-04 09:24:48 -0400

    fixed json links

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const buildLinkList = (myDecks) => {
 	});
 }
 
-fetch(new Request('https://gist.githubusercontent.com/Ocheyo/4d448cb940aa14dc99c7e92f66e075b1/raw/46946b78a4f14619f3be63a745ae710edc374187/myDecks.JSON'))
+fetch(new Request('https://raw.githubusercontent.com/Ocheyo/commander-decks/master/myDecks.JSON'))
   .then(function(response) { 
     response.json().then(function(result) { buildLinkList(result); });
 });
