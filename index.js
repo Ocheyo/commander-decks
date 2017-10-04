0cc439221b3eaccd72cd040a759b3d0bd1dae18a
Jaynes <alex.jaynes@adidas-group.com>
2017-10-04 07:13:27 -0400

    reverted json links

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const buildLinkList = (myDecks) => {
 	});
 }
 
-fetch(new Request('myDecks.JSON'))
+fetch(new Request('https://gist.githubusercontent.com/Ocheyo/4d448cb940aa14dc99c7e92f66e075b1/raw/eeda0fe991d15f119749da115b9175c045243f7e/myDecks.JSON'))
   .then(function(response) { 
     response.json().then(function(result) { buildLinkList(result); });
 });
