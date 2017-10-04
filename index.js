12e5834bafebfa3c9ef254c7ecc197fb6e01a464
Alex Jaynes <dr.ocheyo@gmail.com>
2017-10-03 22:42:34 -0400

    fixed json links

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const buildLinkList = (myDecks) => {
 	});
 }
 
-fetch(new Request('https://gist.githubusercontent.com/Ocheyo/4d448cb940aa14dc99c7e92f66e075b1/raw/083c095392c72a459d7337f60c9ed960e2e39737/myDecks.JSON'))
+fetch(new Request('myDecks.JSON'))
   .then(function(response) { 
     response.json().then(function(result) { buildLinkList(result); });
 });
